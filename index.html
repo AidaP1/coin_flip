b99eb0a7faf103d0b743783dc745026347c62195
Aidan Pilgrim <aidan@aidans-air.home>
2021-02-06 13:20:41 +0000

    added betting options

--- index.html
+++ index.html
@@ -6,8 +6,26 @@
     </header>
     <body>
         <main>
-            <h1 id="text">Flip a coin!</h1>
-                <button id="target">Click me</button>     
+            <h1>Flip a coin!</h1>
+            <form>
+                <div>
+                    <h2>Bet</h2>
+                    <input id="bet" type="number" value="0">
+                </div> 
+                <div>
+                    <h2>Predict</h2>
+                    <input type="radio" id="heads" name="guess" value="Heads" />
+                    <label for="heads">Heads</label><br>
+                    <input type="radio" id="tails" name="guess" value="Tails" />
+                    <label for="tails">Tails</label>
+                </div>
+            </form>
+            <section class="results">
+                <h3 id="round-result">Hold</h3>
+                <p id="rwin">Winnings this round: £0</p>
+                <p id="totwin">Total Winnings: £0</p>
+            </section>
+            <button id="target">Play</button>     
         </main>
         <script src="./script.js"></script>
     </body>
--- script.js
+++ script.js
@@ -1,25 +1,65 @@
 let target = document.getElementById("target");
-let mainText = document.getElementById('text');
+let roundResultText = document.getElementById("round-result");
+let roundWinningText = document.getElementById("rwin");
+let totalWinningText = document.getElementById("totwin");
+
+let betTotal = 0;
+let roundWinnings = 0;
+let totalWinnings = 0;
 
 
 function coinFlip () {
     let coinVal = Math.floor(Math.random() * 2);
     if (coinVal === 1) {
-        mainText.innerHTML = 'Heads';
+        return 'Heads';
     } else {
-        text.innerHTML = 'Tails';
+        return 'Tails';
     }
     target.innerHTML = 'Flip Again'
 }
 
+function getGuess () {
+    const guessValues = document.querySelectorAll('input[name="guess"]');
+    let guess
+    for (const guessChoice of guessValues) {
+        if (guessChoice.checked) {
+            guess = guessChoice.value;
+            return guess;
+        }
+    }   
+}
+
+
+function playGame () {
+    let targetResult = coinFlip();
+    let playerGuess = getGuess();
+    let playerBet = document.getElementById("bet").value;
+    betTotal += playerBet;
+    if (targetResult === playerGuess) {
+        roundWinnings = playerBet * 1.5;
+        let formattedRoundWinnings = new Intl.NumberFormat('en-EN', { style: 'currency', currency: 'GBP' }).format(roundWinnings);
+        roundWinningText.innerHTML = `Winnings this round: ${formattedRoundWinnings}`;
+        totalWinnings += (roundWinnings * 1);
+        let formattedTotalWinnings = new Intl.NumberFormat('en-EN', { style: 'currency', currency: 'GBP' }).format(totalWinnings);
+        totalWinningText.innerHTML = `Total Winnings: ${formattedTotalWinnings}`;
+        roundResultText.innerHTML = `${targetResult} - You won!`;
+        roundResultText.style.visibility = 'visible';
+    } else {
+        roundResultText.innerHTML = `${targetResult} - You Lost!`;
+        roundResultText.style.visibility = 'visible';
+    }
+
+}
+
 function focusOn () {
     target.style.backgroundColor = 'hsl(30 100% 50%)';
+    roundResultText.style.visibility = 'hidden'
 }
 
 function focusOff () {
     target.style.backgroundColor = 'hsl(30 50% 80%)';
 }
 
-target.addEventListener('click',coinFlip);
+target.addEventListener('click',playGame);
 target.addEventListener('mouseenter',focusOn);
 target.addEventListener('mouseleave',focusOff);
